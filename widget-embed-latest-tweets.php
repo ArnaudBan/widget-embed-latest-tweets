4ca07447f07e89ab46653099907c9e27c0063f14
Arnaud <arnaud.banvillet@gmail.com>
2013-09-08 10:15:46 +0200

    New version 0.5

--- widget-embed-latest-tweets.php
+++ widget-embed-latest-tweets.php
@@ -3,7 +3,7 @@
  * Plugin Name: Widget embed latest Tweets
  * Plugin URI: http://arnaudban.me/blog/portfolio/widget-embed-latest-tweets/
  * Description: A Widget to show your latest Tweets. Use the oEmbed methode and some cache. Visit the option page "Plugins->Widget Embed Last Plugin" to authentify yourself
- * Version: 0.4.1
+ * Version: 0.5
  * Author: Arnaud Banvillet
  * Author URI: http://arnaudban.me
  * License: GPL2
