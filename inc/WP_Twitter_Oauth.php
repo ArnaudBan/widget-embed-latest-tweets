3a4eb3c2a3f29f5afc9603481dcee3e1c1d012a5
ArnaudBan <abanvillet@studio-goliath.com>
2017-06-20 16:59:31 +0200

    fixes #11 Start using the WordPress HTTP

---
+++ inc/WP_Twitter_Oauth.php
@@ -0,0 +1,174 @@
+<?php
+
+
+class WP_Twitter_Oauth {
+	public $consumer_key;
+	protected $consumer_secret;
+	public $request;
+	public $params = array();
+	public $cache;
+	public $display_media;
+
+	public function __construct(
+		$Request = 'users/show',
+		$Params = array(),
+		$Cache = 900
+	) {
+
+		$twitter_oauth_var = get_option( 'welt_twitter_oauth_var' );
+
+		if ( $twitter_oauth_var && isset( $twitter_oauth_var['consumer_key'] ) && isset( $twitter_oauth_var['consumer_secret'] ) ) {
+
+			$this->consumer_key    = (string) $twitter_oauth_var['consumer_key'];
+			$this->consumer_secret = (string) $twitter_oauth_var['consumer_secret'];
+			$this->request         = (string) $Request;
+			$this->params          = (array) $Params;
+			$this->cache           = (int) $Cache;
+
+		} else {
+
+			return __( 'The class is not set properly!', 'ttm' );
+
+		}
+
+	}
+
+	/*
+	* Get token from Twitter API 1.1
+	* returns $access_token
+	*/
+	protected function get_access_token() {
+		$credentials = $this->consumer_key . ':' . $this->consumer_secret;
+		$auth        = base64_encode( $credentials );
+		$args        = array(
+			'httpversion' => '1.1',
+			'headers'     => array(
+				'Authorization' => 'Basic ' . $auth,
+				'Content-Type'  => 'application/x-www-form-urlencoded;charset=UTF-8',
+
+				// !important
+
+			),
+			'body'        => array(
+				'grant_type' => 'client_credentials',
+			),
+		);
+
+		$call = wp_remote_post( 'https://api.twitter.com/oauth2/token', $args );
+
+		// need to know what's going on before proceeding
+		if ( 200 == wp_remote_retrieve_response_code( $call ) ) {
+			$keys = json_decode( wp_remote_retrieve_body( $call ) );
+			update_option( md5( $this->consumer_key . $this->consumer_secret ) . '_twitter_access_token', $keys->access_token );
+
+			return __( 'Access granted ^^ !', 'ttm' );
+		} else {
+
+			return $this->check_http_code( wp_remote_retrieve_response_code( $call ) );
+
+		}
+
+	}
+
+
+	/*
+	* Full check
+	* returns $error
+	*/
+	protected function check_http_code( $http_code ) {
+
+		switch ( $http_code ) {
+
+			case '400':
+			case '401':
+			case '403':
+			case '404':
+			case '406':
+				$error = '<div class="error">' . __( 'Your credentials might be unset or incorrect or username is wrong. In any case this error is not due to Twitter API.', 'ttm' ) . '</div>';
+				break;
+
+			case '429':
+				$error = '<div class="error">' . __( 'Rate limits are exceed!', 'ttm' ) . '</div>';
+				break;
+
+			case '500':
+			case '502':
+			case '503':
+				$error = '<div class="error">' . __( 'Twitter is overwhelmed or something bad happened with its API.', 'ttm' ) . '</div>';
+				break;
+
+			default:
+				$error = '<div class="error">' . __( 'Something is wrong or missing. ', 'ttm' ) . '</div>';
+
+		}
+
+		return $error;
+
+	}
+
+	/*
+	* Get object from Twitter API 1.1 with the $access_token
+	* returns $obj from Twitter
+	*/
+	protected function get_obj() {
+		$this->get_access_token();
+		$access_token = get_option( md5( $this->consumer_key . $this->consumer_secret ) . '_twitter_access_token' );
+
+		$args = array(
+			'httpversion' => '1.1',
+			'timeout'     => 120,
+			'headers'     => array(
+				'Authorization' => "Bearer {$access_token}",
+			)
+		);
+
+		$defaults = array(
+			'count' => 1,
+		);
+
+		$q     = "https://api.twitter.com/1.1/{$this->request}.json";
+		$sets  = wp_parse_args( $this->params, $defaults );
+		$query = add_query_arg( $sets, $q );
+
+		$call = wp_remote_get( $query, $args );
+
+		if ( 200 == wp_remote_retrieve_response_code( $call ) ) {
+			$obj = json_decode( wp_remote_retrieve_body( $call ) );
+		} else {
+			$this->delete_cache();
+			$obj = $this->check_http_code( wp_remote_retrieve_response_code( $call ) );
+		}
+
+		return apply_filters( 'the_twitter_object', $obj );
+	}
+
+
+	/*
+	* Get infos but make sure there's some cache
+	* returns (object) $infos from Twitter
+	*/
+	public function get_infos() {
+
+		$set_cache = isset( $this->params ) ? implode( ',', $this->params ) . $this->request : $this->request;
+
+		$cached = unserialize( base64_decode( get_site_transient( md5( $set_cache ) ) ) );// tips with base64_decode props to raherian
+
+		if ( FALSE === $cached ) {
+			$cached = $this->get_obj();
+			set_site_transient( md5( $set_cache ), base64_encode( serialize( $cached ) ), $this->cache );//900 by default because Twitter says every 15 minutes in its doc
+		}
+
+		return $cached;
+	}
+
+
+	/*
+	* Delete cache
+	* In case you need to delete transient
+	*/
+	protected function delete_cache() {
+		$set_cache = isset( $this->params ) ? implode( ',', $this->params ) . $this->request : $this->request;
+		delete_site_transient( md5( $set_cache ) );
+	}
+
+}
--- widget-embed-latest-tweets.php
+++ widget-embed-latest-tweets.php
@@ -29,134 +29,56 @@
 
 
 // Include au php files in the "inc" folder
-foreach (glob( plugin_dir_path( __FILE__ ) . "/inc/*.php" ) as $filename){
-    require_once $filename;
+foreach ( glob( plugin_dir_path( __FILE__ ) . "/inc/*.php" ) as $filename ) {
+	require_once $filename;
 }
 
-add_action('widgets_init', create_function('', 'register_widget( "Widget_Embed_Latest_Tweets" );'));
+add_action( 'widgets_init', create_function( '', 'register_widget( "Widget_Embed_Latest_Tweets" );' ) );
 
-
-/**
-	* Cache the twitter json file. Twitter say "Store API responses in your application or on your site"
-	* And the page load faster with this cache.
-	*
-	* @param array $options
-	* @param boolean $update
-	*/
-function welt_set_tweet_transient( $widget_id, $options ){
-
-
-	$last_tweet = welt_get_latest_tweet( $options );
-
-	if( $last_tweet && is_array( $last_tweet ) ){
-
-		set_transient('last_tweet_' . $widget_id , $last_tweet, 60 * 5);
-
-			foreach ($last_tweet as $tweet_id) {
-
-				$tweet_html = welt_get_tweet_html( $tweet_id, $options );
-
-				if( $tweet_html ){
-					set_transient('last_tweet_html_' . $tweet_id, $tweet_html, ( 24 * WEEK_IN_SECONDS ) ); // 6 mouths
-				}
-
-			}
-	} else {
-
-		delete_transient( 'last_tweet_' . $widget_id );
-	}
-}
-
-/**
- * Get twitter connection based on the option
- *
- */
-function welt_get_twitter_connection(){
-
-	$twitter_oauth_var = get_option('welt_twitter_oauth_var');
-
-	$connection = false;
-	//Check if wee use the authentification methode. We need to have all the key and secret.
-	if( is_array( $twitter_oauth_var ) && count($twitter_oauth_var) == 4 ){
-
-		$connection = new TwitterOAuth($twitter_oauth_var['consumer_key'], $twitter_oauth_var['consumer_secret'], $twitter_oauth_var['token_key'],$twitter_oauth_var['token_secret']);
-
-	}
-
-	return $connection;
-}
 
 /**
  * get the lastest tweet
  */
-function welt_get_latest_tweet( $options ){
+function welt_get_latest_tweet( $options ) {
 
-	$connection = welt_get_twitter_connection();
+	$user_timeline_oaut = new WP_Twitter_Oauth( 'statuses/user_timeline', $options );
+
+	$timeline = $user_timeline_oaut->get_infos();
 
 	// If the connection is ok let get the twitter information
-	if( $connection ){
+	if ( is_array( $timeline ) ) {
 
-		$last_tweet = $connection->get('https://api.twitter.com/1.1/statuses/user_timeline.json', $options );
 
-		// if there is an error
-		if( isset( $last_tweet->errors ) ){
-
-			$return_value = $last_tweet->errors[0]->message;
-			error_log($last_tweet->errors[0]->message);
-
-		// If there is nothing
-		} elseif( $last_tweet == false || empty( $last_tweet )){
-
-			$return_value = false;
-
-		// It should be ok
-		} else {
-
-			$return_value = array();
-			foreach ( $last_tweet as $tweet) {
-				$return_value[] = $tweet->id_str;
-			}
-
+		$return_value = array();
+		foreach ( $timeline as $tweet ) {
+			$return_value[] = $tweet->id_str;
 		}
 
 	} else {
 
-		$return_value = 'Connection error';
+		$return_value = $timeline;
 
 	}
 
 	return $return_value;
 }
 
-function welt_get_tweet_html( $tweet_id, $options ){
+function welt_get_tweet_html( $tweet_id, $options ) {
 
-	$connection = welt_get_twitter_connection();
+	$options['id']          = $tweet_id;
+	$options['omit_script'] = TRUE;
+
+	if ( empty( $maxwidth ) ) {
+		unset( $options['maxwidth'] );
+	}
+
+	$oembed_oaut = new WP_Twitter_Oauth( 'statuses/oembed', $options );
+
+	$oembed = $oembed_oaut->get_infos();
 
 	// If the connection is ok let get the twitter information
-	if( $connection ){
-		$options['id'] = $tweet_id;
-		$options['omit_script'] = true;
+	$return_value = is_string( $oembed ) ? $oembed : $oembed->html;
 
-		if( empty( $maxwidth) ){
-			unset( $options['maxwidth']);
-		}
-
-		$tweet = $connection->get('https://api.twitter.com/1.1/statuses/oembed.json', $options);
-
-		if( isset( $tweet->errors ) ){
-
-			$return_value = 'false';
-			error_log( $last_tweet->errors[0]->message );
-
-		} else {
-
-			$return_value = $tweet->html;
-		}
-
-	} else {
-
-		$return_value = 'false';
-	}
 
 	return $return_value;
 }
@@ -165,133 +87,74 @@ function welt_get_tweet_html( $tweet_id, $options ){
 /**
  * The function callback in ajax to display Tweet
  */
-function welt_display_tweets( ){
+function welt_display_tweets() {
 
 	$all_welt_widgets = $_POST['all_welt_widgets'];
 
 	$tweet_html = array();
 
-	foreach ($all_welt_widgets as $welt_widget ) {
+	foreach ( $all_welt_widgets as $welt_widget ) {
 
-		$widget_id = $welt_widget['widget_id'];
-		$widget_data = isset( $welt_widget['widget_data'] ) && ! empty( $welt_widget['widget_data'] ) ? $welt_widget['widget_data'] : false;
+		$widget_id   = $welt_widget['widget_id'];
+		$widget_data = isset( $welt_widget['widget_data'] ) && ! empty( $welt_widget['widget_data'] ) ? $welt_widget['widget_data'] : FALSE;
 
 		$tweet_html[ $widget_id ] = '';
 
 		// In preview mode we passe all instance in data html attrribut
-		if( $widget_data ){
+		$instance = $widget_data ? $widget_data : welt_get_widget_instance( $widget_id );
 
-			$instance = $widget_data;
+		$last_tweet = welt_get_latest_tweet( $instance );
 
-			$last_tweet = welt_get_latest_tweet( $instance );
+		foreach ( $last_tweet as $tweet_id ) {
 
-			foreach ($last_tweet as $tweet_id) {
-
-				$tweet_html[ $widget_id ] .= welt_get_tweet_html( $tweet_id, $instance );
-
-			}
-
-
-		} else {
-
-
-			$last_tweet = get_transient('last_tweet_' . $widget_id);
-
-			if( false === $last_tweet ) {
-
-				// Get the widget instance
-				$instance = $instance = welt_get_widget_instance( $widget_id );
-
-
-				// Set the transient for this widget
-				$last_tweet = welt_get_latest_tweet( $instance );
-			}
-
-			if( $last_tweet && is_array( $last_tweet ) ){
-
-				set_transient('last_tweet_' . $widget_id , $last_tweet, 60 * 5);
-
-				foreach ($last_tweet as $tweet_id) {
-
-					// retrocompatibility
-					if( is_object( $tweet_id ) ){
-						$tweet_id = $tweet_id->id_str;
-					}
-
-					$tweet_html_transient = get_transient('last_tweet_html_' . $tweet_id);
-
-					if( false === $tweet_html_transient ){
-
-						$instance = welt_get_widget_instance( $widget_id );
-
-						$tweet_html_transient = welt_get_tweet_html( $tweet_id, $instance );
-
-						if( $tweet_html_transient ){
-							set_transient('last_tweet_html_' . $tweet_id, $tweet_html_transient, ( 24 * WEEK_IN_SECONDS ) ); // 6 mouths
-							$tweet_html[ $widget_id ] .= $tweet_html_transient;
-						}
-
-					} else {
-
-						// retrocompatibility
-						if( is_object( $tweet_html_transient ) ){
-							$tweet_html_transient = $tweet_html_transient->html;
-						}
-						$tweet_html[ $widget_id ] .= $tweet_html_transient;
-					}
-
-				}
-			}
+			$tweet_html[ $widget_id ] .= welt_get_tweet_html( $tweet_id, $instance );
 
 		}
 	}
-
 
 	echo json_encode( $tweet_html );
 	die;
 }
 
-add_action('wp_ajax_welt_display_tweets', 'welt_display_tweets');
-add_action('wp_ajax_nopriv_welt_display_tweets', 'welt_display_tweets');
+add_action( 'wp_ajax_welt_display_tweets', 'welt_display_tweets' );
+add_action( 'wp_ajax_nopriv_welt_display_tweets', 'welt_display_tweets' );
 
 
 /**
  * Return the instance of a widget base on his id
  */
-function welt_get_widget_instance( $widget_id ){
+function welt_get_widget_instance( $widget_id ) {
 
 	// Get the widget instance
-	$all_instance_widget = get_option('widget_welt_last_tweets');
+	$all_instance_widget = get_option( 'widget_welt_last_tweets' );
 
-	$widget_real_id = str_replace('welt_last_tweets-', '', $widget_id);
+	$widget_real_id = str_replace( 'welt_last_tweets-', '', $widget_id );
 
-	return $all_instance_widget[$widget_real_id];
+	return $all_instance_widget[ $widget_real_id ];
 }
 
 /**
  * Enqueue welt script and Twitter Script
  */
-function welt_enqueue_scripts(){
+function welt_enqueue_scripts() {
 	// welt
-	wp_register_script('welt_twitter_script', '//platform.twitter.com/widgets.js' , array(), '1.1', true );
-	wp_register_script('welt_script', plugins_url('/js/welt-scripts.js', __FILE__) , array( 'jquery', 'welt_twitter_script' ), '20140723', true );
+	wp_register_script( 'welt_twitter_script', '//platform.twitter.com/widgets.js', array(), '1.1', TRUE );
+	wp_register_script( 'welt_script', plugins_url( '/js/welt-scripts.js', __FILE__ ), array(
+		'jquery',
+		'welt_twitter_script'
+	), '20140723', TRUE );
 
 }
-add_action('wp_enqueue_scripts', 'welt_enqueue_scripts');
+
+add_action( 'wp_enqueue_scripts', 'welt_enqueue_scripts' );
 
 function welt_plugin_init() {
 
-	//Files needed for the Twitter authentification
-	//Check if TwitterOAuth doesn't already existe
-	if( ! class_exists( 'TwitterOAuth' )){
 
-		require_once 'twitteroauth/twitteroauth.php';
-
-	}
-
-  load_plugin_textdomain( 'widget-embed-lastest-tweets', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'widget-embed-lastest-tweets', FALSE, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
-add_action('plugins_loaded', 'welt_plugin_init');
+
+add_action( 'plugins_loaded', 'welt_plugin_init' );
 
 
 // Option page
@@ -299,11 +162,12 @@ require_once 'welt-option.php';
 
 
 // Add settings link on plugin page
-function welt_add_settings_link($links) {
-  $settings_link = '<a href="options-general.php?page=welt_options_page">' . __('Settings') . '</a>';
-  array_unshift($links, $settings_link);
-  return $links;
+function welt_add_settings_link( $links ) {
+	$settings_link = '<a href="options-general.php?page=welt_options_page">' . __( 'Settings' ) . '</a>';
+	array_unshift( $links, $settings_link );
+
+	return $links;
 }
 
-$plugin = plugin_basename(__FILE__);
-add_filter("plugin_action_links_$plugin", 'welt_add_settings_link' );
+$plugin = plugin_basename( __FILE__ );
+add_filter( "plugin_action_links_$plugin", 'welt_add_settings_link' );
